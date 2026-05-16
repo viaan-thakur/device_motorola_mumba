b80eef4e10a9c7db17a0c52d3075ca9eeae44c59
zaraknyu <razenxizu@gmail.com>
2026-05-16 20:29:42 +0000

    mumba: Import WiFi configs into tree
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

--- device.mk
+++ device.mk
@@ -443,6 +443,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/adrastea/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/adrastea/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
         firmware_adrastea_wlan_mac.bin_symlink \
