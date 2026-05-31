a2c47831777480a140124016cb8aedf9efba14a0
zaraknyu <razenxizu@gmail.com>
2026-05-31 12:28:32 +0000

    mumba: Drop lineage health service

--- device.mk
+++ device.mk
@@ -226,15 +226,6 @@ PRODUCT_PACKAGES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/device/force_charging_disable)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
-
 # Moto hardware
 PRODUCT_PACKAGES += \
     MotoActions \
