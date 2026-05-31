14a5b5a520e9a1b76f7cb266040e0d92cfbbce8a
ZaraKnYu <razenxizu@gmail.com>
2026-05-31 11:23:42 +0530

    mumba: Switch charging control node to force_charging_disable

--- device.mk
+++ device.mk
@@ -225,7 +225,7 @@ PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_current_limit)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/device/force_charging_disable)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
